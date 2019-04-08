2111bba2c86d615940a1b754b6d20c13c5f50527
ikda <akusiron@gmail.com>
2019-04-08 23:19:23 +0900

    #1.1 setup and old way

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component, Fragment } from 'react';
+import "./App.css"
+
+class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <Button danger />
+        <Button />
+      </Fragment>
+      
+    );
+  }
+}
+
+const Button = ({danger}) => (
+  <button
+    className={danger? "button button-success":"button button-danger"}
+  >
+  Hello!
+  </button>
+);
+
+
+export default App;
