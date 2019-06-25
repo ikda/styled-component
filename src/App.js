22f31dfa272b1c2ae79b09a743638d991b3d9bd3
EunSoo Lee <akusiron@gmail.com>
2019-06-25 15:39:27 +0900

    #5 extra attrs and Mixins

--- src/App.js
+++ src/App.js
@@ -1,21 +1,5 @@
 import React, { Component } from "react";
-import styled, { createGlobalStyle, css, keyframes } from "styled-components";
-
-const rotation = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-const GlobalStyle = createGlobalStyle`
-  body{
-    padding: 0;
-    margin: 0;
-  }
-`;
+import styled, { css } from "styled-components";
 
 const Container = styled.div`
 	height: 100vh;
@@ -23,48 +7,26 @@ const Container = styled.div`
 	background-color: #95a5a6;
 `;
 
-const Button = styled.button`
-	border-radius: 50px;
-	padding: 5px;
-	min-width: 120px;
-	color: white;
-	font-weight: 600;
-	-webkit-appearance: none;
-	cursor: pointer;
-	&:active,
-	&:focus {
-		outline: none;
-	}
-	background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
-
-	${props => {
-		if (props.danger) {
-			return css`
-				animation: ${rotation} ${props.rotationTime}s linear infinite;
-			`;
-		}
-	}}
+const awesomeCard = css`
+  box-shadow 0 4px 6px rgba(50,50,93,0.11), 0 1px 3px rgba(0,0,0, 0.08);
+  background-color: white;
+  border-radius: 10px;
+  padding: 20px;
 `;
 
-const Anchor = styled(Button)`
-	text-decoration: none;
+const Input = styled.input.attrs({
+	required: true
+})`
+	border-radius: 5px;
+	${awesomeCard};
 `;
 
 class App extends Component {
 	render() {
 		return (
-			<>
-				<GlobalStyle />
-				<Container>
-					<Button>Hello</Button>
-					<Button danger rotationTime={60}>
-						Hello
-					</Button>
-					<Anchor as="a" href="https://www.google.com">
-						Go to Google
-					</Anchor>
-				</Container>
-			</>
+			<Container>
+				<Input placeholder="hello" />
+			</Container>
 		);
 	}
 }
