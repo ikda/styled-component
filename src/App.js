7fc1aa46be8d60c0dc054cc6b02092592b9c7576
EunSoo Lee <akusiron@gmail.com>
2019-06-25 16:05:31 +0900

    #6 theme

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import styled, { css } from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "./theme";
 
 const Container = styled.div`
 	height: 100vh;
@@ -7,28 +8,32 @@ const Container = styled.div`
 	background-color: #95a5a6;
 `;
 
-const awesomeCard = css`
-  box-shadow 0 4px 6px rgba(50,50,93,0.11), 0 1px 3px rgba(0,0,0, 0.08);
-  background-color: white;
-  border-radius: 10px;
-  padding: 20px;
+const Card = styled.div`
+	background-color: red;
 `;
 
-const Input = styled.input.attrs({
-	required: true
-})`
-	border-radius: 5px;
-	${awesomeCard};
+const Button = styled.button`
+	border-radius: 30px;
+	padding: 25px 15px;
+	background-color: ${props => props.theme.successColor};
 `;
 
 class App extends Component {
 	render() {
 		return (
-			<Container>
-				<Input placeholder="hello" />
-			</Container>
+			<ThemeProvider theme={theme}>
+				<Container>
+					<Form />
+				</Container>
+			</ThemeProvider>
 		);
 	}
 }
+
+const Form = () => (
+	<Card>
+		<Button> Hello </Button>
+	</Card>
+);
 
 export default App;
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+	mainColor: "#8e44ad",
+	dangerColor: "#e74c3c",
+	successColor: "#2ecc71"
+};
+
+export default theme;
